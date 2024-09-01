acb8e82709ce186b9dcaa5df2b43df022875a7d2
eamonnhanson <eamonn_hanson@yahoo.co.uk>
2024-09-01 11:03:32 +0200

    in between save

---
+++ exercise/model/gameData.js
@@ -0,0 +1,13 @@
+const xp=0;
+const health=100;
+const gold =50;
+const currentWeapon=0;
+const fighting;
+const monsterHealth;
+const inventory = [“stick”];
+const weapons = [
+    {name: "stick", power: 5},
+    {name: "dagger", power: 30},
+    {name: "claw hammer", power: 50},
+    {name: "sword", power: 100}
+];
